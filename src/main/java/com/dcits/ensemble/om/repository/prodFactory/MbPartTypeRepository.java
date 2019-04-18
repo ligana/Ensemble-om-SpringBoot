f766217ab897e13c9b2914fea667e37e6ff327e4
xiaopan <xiaopan@dcits.com>
2019-04-18 19:38:02 +0800

    指标分类和指标类型的实体类服务层，持久层

---
+++ src/main/java/com/dcits/ensemble/om/repository/prodFactory/MbPartTypeRepository.java
@@ -0,0 +1,18 @@
+package com.dcits.ensemble.om.repository.prodFactory;
+
+import com.dcits.ensemble.om.model.dbmodel.tables.MbPartType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+/**
+ * @ClassName MbPartTypeRepository
+ * @Author xiaopan
+ * @Date 2019/4/18 19:08
+ * @Description TODO
+ * @Version 1.0
+ **/
+public interface MbPartTypeRepository extends JpaRepository<MbPartType,Long>,JpaSpecificationExecutor<MbPartType> {
+    List<MbPartType> findAll();
+}
